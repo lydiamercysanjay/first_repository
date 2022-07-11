8a094edb448f24314e241dc7289a044ac1cc88e1
lydiamercysanjay <lydiamercysanjay@gmail.com>
2022-07-11 23:39:34 +0530

    modified Exercise01_o1.java file

--- ch_01/Exercise01_01.java
+++ ch_01/Exercise01_01.java
@@ -1,5 +1,7 @@
 package ch_01;
 
+
+
 /**
  * 1.1 (Display three messages) Write a program that displays Welcome to Java,
  * Welcome to Computer Science, and Programming is fun.
@@ -8,6 +10,7 @@ public class Exercise01_01 {
     public static void main(String[] args) {
 
         System.out.println("Welcome to Java");
+System.out.println("Done changes by Lydia");
         System.out.println("Welcome to Computer Science");
         System.out.println("Programming is fun");
     }
